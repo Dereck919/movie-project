66e0e2f4e1a5aa3cd589c0f58e62a6c3b61ce1f1
ibr0che <ibroshe5@gmail.com>
2025-11-03 20:14:31 -0800

    starting on homepage desgin

---
+++ frontend/src/components/common/Moviecard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Moviecard() {
+  return <div class="flex flex-col bg-amber-500 p-10"></div>;
+}
+
+export default Moviecard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Loading from "../components/common/Loading.jsx";
+import Moviecard from "../components/common/Moviecard.jsx";
 
 function Home() {
   return (
-    <div class="flex-col justify-center">
-      <h1>Now Playing</h1>
-      <div class="grid grid-cols-3 gap-4">
+    <div class="flex flex-col">
+      <h1 class="text-center">Now Playing</h1>
+      <div class="grid grid-cols-6 gap-8 p-8">
+        <Moviecard />
+        <Moviecard />
         <div class="border-2 rounded-md">01</div>
         <div class="border-2 rounded-md">02</div>
         <div class="border-2 rounded-md">03</div>
-        <div class="col-span-2 border-2 rounded-md">04</div>
+        <div class="border-2 rounded-md">04</div>
         <div class="border-2 rounded-md">05</div>
+        <Moviecard />
         <div class="border-2 rounded-md">06</div>
-        <div class="col-span-2 border-2 rounded-md">07</div>
+      </div>
+      <div class="grid grid-cols-4 gap-20 m-8 border-2 p-8">
+        <div class="border-2 rounded-md">Movie 01</div>
+        <div class="border-2 rounded-md">Movie 02</div>
+        <div class="border-2 rounded-md">Movie 03</div>
+        <div class="border-2 rounded-md">Movie 04</div>
+        <div class="border-2 rounded-md">Movie 05</div>
+        <div class="border-2 rounded-md">Movie 06</div>
+        <div class="border-2 rounded-md">Movie 01</div>
+        <div class="border-2 rounded-md">Movie 02</div>
+        <div class="border-2 rounded-md">Movie 03</div>
+        <div class="border-2 rounded-md">Movie 04</div>
+        <div class="border-2 rounded-md">Movie 05</div>
+        <div class="border-2 rounded-md">Movie 06</div>
       </div>
       <Loading />
     </div>
