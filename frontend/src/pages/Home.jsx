ae130b913fa38079ca158b1f36347470573c5f39
ibr0che <ibroshe5@gmail.com>
2025-11-05 17:32:05 -0800

    merging movie-card branch to main

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Loading from "../components/common/Loading.jsx";
+import MovieCard from "../components/common/Moviecard.jsx";
 
 function Home() {
   return (
     <div class="flex-col justify-center">
       <h1>Now Playing</h1>
-      <div class="grid grid-cols-3 gap-4">
-        <div class="border-2 rounded-md">01</div>
-        <div class="border-2 rounded-md">02</div>
-        <div class="border-2 rounded-md">03</div>
-        <div class="col-span-2 border-2 rounded-md">04</div>
-        <div class="border-2 rounded-md">05</div>
-        <div class="border-2 rounded-md">06</div>
-        <div class="col-span-2 border-2 rounded-md">07</div>
+      <div class="grid grid-cols-2  gap-4">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
       </div>
-      <Loading />
     </div>
   );
 }
