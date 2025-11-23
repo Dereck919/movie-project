c8bc3dc99a40a1c5a13a3e76f91034ec0abc6965
DjRePrincess <148670490+DjRePrincess@users.noreply.github.com>
2025-11-22 22:11:05 -0800

    made all the ticket components more palatable

--- frontend/src/components/common/TicketInformation.jsx
+++ frontend/src/components/common/TicketInformation.jsx
@@ -3,9 +3,9 @@ import Ticket from "./Ticket";
 
 function TicketInformation() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="py-20 bg-black mx-8 rounded-lg p-4">
-        <h2 className="text-white text-3xl font-semibold">My Tickets</h2>
+    <div className="my-6 flex flex-col min-h-screen">
+      <div className="mx-4 rounded-lg p-4">
+        <h2 className="text-white text-3xl font-semibold ">My Tickets</h2>
       </div>
       <div className="">
         <Ticket
